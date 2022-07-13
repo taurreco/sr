923db74a7fa7e93e67e4b9ea43e028c162068bd3
taurreco <tomasdougan@gmail.com>
2022-07-13 03:21:38 -0400

    simplified clip logic? maybe? still looks bad

--- src/sr_clip.c
+++ src/sr_clip.c
@@ -5,43 +5,15 @@
 #include "sr_clip.h"
 
 int
-clip_axis(float* src, 
-          float* dest, 
-          size_t* num_pts, 
-          size_t num_attr, 
-          size_t axis)
+clip_triangle(float* src, float* dest, size_t* num_pts, size_t num_attr)
 {
-    float tmp[16 * 32];
-    if (!clip_routine(src, tmp, num_pts, num_attr, axis, 1)) {
-        return 0;
-    }
-    if (!clip_routine(tmp, dest, num_pts, num_attr, axis, -1)) {
-        return 0;
-    }
-    return 1;
-}
-
-int
-clip(float* src, float* dest, size_t* num_pts, size_t num_attr)
-{
-    /*
-    int v0_inside = in_frustum(src + 0 * num_attr);
-    int v1_inside = in_frustum(src + 1 * num_attr);
-    int v2_inside = in_frustum(src + 2 * num_attr);
-    
-
-    if (v0_inside && v1_inside && v2_inside) {
-        return 1;
-    }
-
-    if (!v0_inside && !v1_inside && !v2_inside) {
-        return 0;
-    }
-    */
-    float tmp[16 * 32];
-    return clip_axis(src, dest, num_pts, num_attr, 0) &&
-           clip_axis(dest, tmp, num_pts, num_attr, 1) &&
-           clip_axis(tmp, dest, num_pts, num_attr, 2);
+    float tmp[16 * SR_MAX_ATTRIBUTE_COUNT];
+    return clip_routine(src, tmp, num_pts, num_attr, 0, 1) &&
+           clip_routine(tmp, dest, num_pts, num_attr, 0, -1) &&
+           clip_routine(dest, tmp, num_pts, num_attr, 1, 1) &&
+           clip_routine(tmp, dest, num_pts, num_attr, 1, -1) &&
+           clip_routine(dest, tmp, num_pts, num_attr, 2, 1) &&
+           clip_routine(tmp, dest, num_pts, num_attr, 2, -1);
 }
 
 
@@ -55,9 +27,12 @@ clip_routine(float* src,
 {
     size_t k = 0;
     int poly_inside = 0;
+
     float* prev = src + (*num_pts - 1) * num_attr;  /* last pt */
     int prev_inside = prev[axis] * sign <= prev[3];
+
     for (size_t i = 0; i < *num_pts; i++) {
+
         float* curr = src + i * num_attr;
         int curr_inside = curr[axis] * sign <= curr[3];
 
@@ -74,6 +49,7 @@ clip_routine(float* src,
             k++;
             poly_inside = 1;
         }
+        
         prev = curr;
         prev_inside = curr_inside;
     }
